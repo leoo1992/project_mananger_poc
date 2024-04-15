af016ab614c6b70fa13243ae5020fe96afc7b98b
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-04-15 14:39:00 -0300

    added elements inhome page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,12 @@
+import { Link } from 'react-router-dom';
+import ImgBg from '../components/ImgContainer/ImgBg';
+
 export default function HomePage() {
   return (
-    <section className="flex flex-col w-full">
+    <section
+      className="flex flex-col flex-grow w-full h-full
+     self-center items-center justify-center content-center text-center py-10 sm:py-16"
+    >
       <h1 className="text-xl sm:text-2xl">
         Bem-vindo ao
         <span className="text-warning p-1">Costs</span>
@@ -8,7 +14,13 @@ export default function HomePage() {
       <p className="pt-4 text-sm sm:text-xl">
         Começe a gerenciar seus projetos
       </p>
-      <a href=""></a>
+      <Link
+        to="/projects"
+        className="btn btn-warning btn-sm sm:btn-md w-32 flex self-center items-center mt-6 border-0"
+      >
+        Criar Projeto
+      </Link>
+      <ImgBg />
     </section>
   );
 }
