ce293631ed66ee85d889e8011bbbd717c855ffea
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-04-16 15:33:30 -0300

    deps config and update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+  },
   plugins: [
     react(),
-    reactRefresh(),
     replace({
       preventAssignment: true,
     }),
