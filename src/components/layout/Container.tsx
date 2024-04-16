9bd3d4e3698e4c949ee310eb0ab844bc36497346
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-04-16 10:57:52 -0300

    desestruturação

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,32 +1,16 @@
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactNode,
-  ReactPortal,
-} from 'react';
+import { ReactNode } from 'react';
 
 type Props = {
-  children:
-    | ReactNode
-    | string
-    | number
-    | boolean
-    | ReactElement<unknown, string | JSXElementConstructor<unknown>>
-    | Iterable<ReactNode>
-    | ReactPortal
-    | null
-    | undefined;
+  children: ReactNode;
   customClass?: string;
 };
-export default function Container(props: Props) {
+export default function Container({ children, customClass = '' }: Props) {
   return (
     <main
       className={`flex
-    items-start min-h-screen min-w-screen flex-wrap py-20 px-4 ${
-      props.customClass ? props.customClass : ''
-    }`}
+    items-start min-h-screen min-w-screen flex-wrap py-20 px-4 ${customClass}`}
     >
-      {props.children}
+      {children}
     </main>
   );
 }
