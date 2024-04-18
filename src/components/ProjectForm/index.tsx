e78dad3310ca73efa7be01b0ac8121e9c4d695fc
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-04-18 18:13:36 -0300

    refactor components and pages

--- src/components/ProjectForm/index.tsx
+++ src/components/ProjectForm/index.tsx
@@ -1,77 +1,42 @@
+import { useProjectForm } from '../../hooks/useProjectForm';
+import FormEffects from '../../utils/FormEffects';
+import { submitForm_CreateProject } from '../../utils/submitForm_CreateProject';
 import LinkButton from '../layout/LinkButton';
 import Form from './SubComponents/Form';
 import Input from './SubComponents/Input';
 import Select from './SubComponents/Select';
-import { useRef, useState, useEffect } from 'react';
 
 export default function ProjectForm({
   btnText,
   handleSubmit,
-  projectData,
 }: {
   btnText: string;
   handleSubmit: (project: any) => void;
-  projectData: any;
 }) {
-  const NameRef = useRef<HTMLInputElement>(null);
-  const OrçamentoRef = useRef<HTMLInputElement>(null);
-  const SelectRef = useRef<HTMLSelectElement>(null);
-  const [error, setError] = useState<boolean | null | undefined>(false);
-  const [options, setOptions] = useState<Promise<any> | any>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:5000/categories', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        if (SelectRef.current) {
-          setOptions(data);
-        }
-      })
-      .catch((er) => console.log(er));
-    NameRef.current?.focus();
-  }, []);
-
-  const submit = (e: any) => {
-    e.preventDefault();
-    if (validateForm()) {
-      const { name, cost, category } = {
-        name: NameRef.current?.value,
-        cost: OrçamentoRef.current?.value,
-        category: [
-          SelectRef.current?.value,
-          SelectRef.current?.selectedOptions[0].textContent,
-        ],
-      };
-      handleSubmit({ name, cost, category });
-      e.target.reset();
-      NameRef.current?.focus();
-    }
-  };
-
-  const validateForm = () => {
-    if (
-      NameRef.current?.value &&
-      OrçamentoRef.current?.value &&
-      SelectRef.current?.value &&
-      SelectRef.current?.value !== '0' &&
-      OrçamentoRef.current?.value.trim() !== '' &&
-      NameRef.current?.value.trim() !== '' &&
-      SelectRef.current?.value.trim() !== ''
-    ) {
-      setError(false);
-      return true;
-    }
-    setError(true);
-    return false;
-  };
+  const {
+    NameRef,
+    OrçamentoRef,
+    SelectRef,
+    error,
+    options,
+    handleSubmit: submitForm,
+    setError,
+    setOptions,
+  } = useProjectForm();
 
   return (
-    <Form onSubmit={submit}>
+    <Form
+      onSubmit={(e: any) =>
+        submitForm_CreateProject(
+          e,
+          NameRef,
+          OrçamentoRef,
+          SelectRef,
+          setError,
+          handleSubmit
+        )
+      }
+    >
       <div className="flex flex-col w-full max-w-xs p-4 gap-y-1">
         <Input
           inputRef={NameRef}
@@ -84,6 +49,7 @@ export default function ProjectForm({
           labelName={'Orçamento'}
           typeInput="number"
         />
+        <FormEffects SelectRef={SelectRef} setOptions={setOptions} />
         <Select
           selectRef={SelectRef}
           nameID={'categoria_id'}
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -2,22 +2,13 @@ import { BiHighlight } from 'react-icons/bi';
 import { HiTrash } from 'react-icons/hi';
 import { Key, useEffect, useState } from 'react';
 import LinkButton from '../components/layout/LinkButton';
+import readAllProjects from '../services/Projects/readAllProjects';
 
 export default function ProjectsPage() {
   const [projects, setProjects] = useState<Promise<any> | any>([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/projects', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        setProjects(data);
-      })
-      .catch((er) => console.log(er));
+    readAllProjects(setProjects);
   }, []);
 
   return (
