143aea3a980678e81ad1d467f37130fcec243c8c
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-04-18 18:12:25 -0300

    create custom hooks

---
+++ src/hooks/useProjectForm.tsx
@@ -0,0 +1,32 @@
+import { useRef, useState } from 'react';
+import { submitForm_CreateProject } from '../utils/submitForm_CreateProject';
+
+export function useProjectForm() {
+  const NameRef = useRef<HTMLInputElement>(null);
+  const OrçamentoRef = useRef<HTMLInputElement>(null);
+  const SelectRef = useRef<HTMLSelectElement>(null);
+  const [error, setError] = useState<boolean | null | undefined>(false);
+  const [options, setOptions] = useState<Promise<any> | any>([]);
+
+  const handleSubmit = (e: any) => {
+    submitForm_CreateProject(
+      e,
+      NameRef,
+      OrçamentoRef,
+      SelectRef,
+      setError,
+      handleSubmit
+    );
+  };
+
+  return {
+    NameRef,
+    OrçamentoRef,
+    SelectRef,
+    error,
+    options,
+    handleSubmit,
+    setOptions,
+    setError,
+  };
+}
