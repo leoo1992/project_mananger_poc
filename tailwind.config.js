b62b3d27176bbb9affd3c6ab23dc1f6779c95b61
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-04-22 09:30:29 -0300

    remove unused coments

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {},
   },
-  // eslint-disable-next-line no-undef
+
   plugins: [require('daisyui')],
   daisyui: {
     themes: ['light', 'dark'],
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 5173,
